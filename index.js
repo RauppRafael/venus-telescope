d15ed23182b29d6554078667b458832d49d15f08
Raupp <raupperino@gmail.com>
2021-03-02 13:17:18 -0300

    Improved venus code modularity

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 require('dotenv').config()
 
-const mail = require('./src/Mail')
 const venus = require('./src/Venus')
 
 const main = async () => {
-    const usage = await venus.getBorrowUsage()
-    const subject = (usage * 100).toFixed(2) + '% is your current collateral usage'
-
-    mail.send({
-        to: process.env.MAILGUN_DANGER_EMAIL || process.env.MAILGUN_WARNING_EMAIL,
-        subject,
-        message: subject
-            + '<br><br>'
-            + 'To check it out access <a href="https://app.venus.io">app.venus.io</a>',
+    await venus.handle({
+        wallet: process.env.WALLET_ADDRESS,
+        yieldWatchApiUrl: process.env.YIELDWATCH_API_URL,
+        warningEmail: process.env.MAILGUN_DANGER_EMAIL,
+        dangerEmail: process.env.MAILGUN_WARNING_EMAIL,
     })
 }
 
--- src/Venus.js
+++ src/Venus.js
@@ -1,16 +1,35 @@
 const axios = require('axios')
-
-const getVenusAccount = async () => {
-    const url = process.env.YIELDWATCH_API_URL + process.env.WALLET_ADDRESS + '?platforms=venus'
-
-    return (await axios(url)).data.result.Venus
-}
+const mail = require('./Mail')
 
 module.exports = {
-    async getBorrowUsage() {
-        const venus = await getVenusAccount()
-        const values = venus.supplyBorrowData.totalUSDValues
+    async handle({ wallet, yieldWatchApiUrl, warningEmail, dangerEmail }) {
+        const account = await this.account({ wallet, yieldWatchApiUrl })
+        const usage = await this.getBorrowUsage({ account })
 
-        return values.borrow / (values.supply * 0.6)
+        await this.sendNotification({ usage, warningEmail, dangerEmail })
+    },
+
+    async account({ wallet, yieldWatchApiUrl }) {
+        const url = yieldWatchApiUrl + wallet + '?platforms=venus'
+
+        return (await axios(url)).data.result.Venus
+    },
+
+    async getBorrowUsage({ account }) {
+        const { borrow, supply } = account.supplyBorrowData.totalUSDValues
+
+        return borrow / (supply * 0.6)
+    },
+
+    async sendNotification({ usage, warningEmail, dangerEmail }) {
+        const subject = (usage * 100).toFixed(2) + '% is your current collateral usage'
+
+        mail.send({
+            to: dangerEmail || warningEmail,
+            subject,
+            message: subject
+                + '<br><br>'
+                + 'To check it out access <a href="https://app.venus.io">app.venus.io</a>',
+        })
     },
 }
